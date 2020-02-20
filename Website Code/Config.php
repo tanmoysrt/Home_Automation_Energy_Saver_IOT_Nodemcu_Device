c5f474092e1f725f514692d843a29fe3f1d40405
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2020-02-20 23:39:31 +0530

    Update Config.php

--- Website Code/Config.php
+++ Website Code/Config.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect("localhost","id11628615_data","sarkar74112715072002","id11628615_data",3306);
+	$con = mysqli_connect("localhost","id11628615_data","password","id11628615_data",3306);
 	if (!$con)
 	{
 	die('Could not connect: ' . mysql_error());
